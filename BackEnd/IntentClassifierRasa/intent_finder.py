f989c110e98686b3937c998324790382055f1484
Sandaruth <siriwardanahbks.20@uom.lk>
2023-08-31 20:21:27 +0530

    temp

--- BackEnd/IntentClassifierRasa/intent_finder.py
+++ BackEnd/IntentClassifierRasa/intent_finder.py
@@ -3,15 +3,16 @@ import json
 
 url = "http://localhost:5005/model/parse"
 
-def nlu_respond(phrase: str):
+def get_intent(phrase: str):
     nlu_data = json.dumps({'text': f"{phrase}"})
     nlu_resp = requests.post(url, data=nlu_data).json()
-    return nlu_resp
+    if nlu_resp['intent']['confidence'] > 0.7:
+        return nlu_resp['intent']['name']
+    else:
+        return "intent not found"
 
 def run_test():
     url = "http://localhost:5005/model/parse"
     obj = {"text": "Hi?"}
     response = requests.post(url, data=json.dumps(obj))
     print(response.json())
-
-print(nlu_respond("Hi"))
