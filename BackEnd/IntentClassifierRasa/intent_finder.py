72965e6c0516df6acc3d75d0fd9f84c080b4518d
Sandaruth <siriwardanahbks.20@uom.lk>
2023-09-01 12:24:52 +0530

    rasa data set update

--- BackEnd/IntentClassifierRasa/intent_finder.py
+++ BackEnd/IntentClassifierRasa/intent_finder.py
@@ -6,13 +6,13 @@ url = "http://localhost:5005/model/parse"
 def get_intent(phrase: str):
     nlu_data = json.dumps({'text': f"{phrase}"})
     nlu_resp = requests.post(url, data=nlu_data).json()
-    if nlu_resp['intent']['confidence'] > 0.7:
+    if nlu_resp['intent']['confidence'] > 0.5:
         return nlu_resp['intent']['name']
     else:
         return "intent not found"
 
 def run_test():
     url = "http://localhost:5005/model/parse"
-    obj = {"text": "Hi?"}
+    obj = {"text": "do you know my accont type"}
     response = requests.post(url, data=json.dumps(obj))
     print(response.json())
