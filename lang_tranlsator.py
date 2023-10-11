d30de90ecad786e5ff016b128a38b5f465184506
Sanujen <sanuprem6@gmail.com>
2023-10-11 14:19:10 +0530

    updates on translators

--- lang_tranlsator.py
+++ lang_tranlsator.py
@@ -3,6 +3,8 @@ from pprint import pprint
 
 
 def translate_to_lang(message: str, translate_from: str, translate_to: str):
+    if translate_from == translate_to:
+        return message
     translator = Translator()
     translation = translator.translate(message, dest=translate_to, src=translate_from)
     print(translation.text)
