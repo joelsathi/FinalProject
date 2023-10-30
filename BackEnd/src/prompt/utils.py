a01d283dca3f8bfc0388bbbae802d2ad91d2c706
joelsathi <joelsathiyendra@gmail.com>
2023-10-30 18:57:25 +0530

    updated the routes utils file

--- BackEnd/src/prompt/utils.py
+++ BackEnd/src/prompt/utils.py
@@ -1,32 +1,42 @@
-from ...LLM.llm_out import generate_llama2_response
-from ...VectorDB_chat.access_db import search_similarity
-from ...FireBaseDB.access_db import GetAccountDetails
-from ...IntentClassifierRasa.intent_finder import get_intent
+from LLM.llm_out import generate_llama2_response
+from VectorDB_chat.access_db import search_similarity
+from FireBaseDB.access_db import GetAccountDetails
+from IntentClassifierRasa.intent_finder import get_intent
+from fastapi.responses import JSONResponse
 
-async def get_response(user_msg, token):
+def get_response(user_msg, token):
+
+    # return user_msg
 
     intent = get_intent(user_msg)
+
+    # return intent
 
     db_context = ""
     vec_db_context = ""
 
-    if intent == "Account_details":
-        db_context = GetAccountDetails(token['localId'])
-    elif intent == "Transaction_history":
+    if intent == "Account_details" or intent == "Account_balance":
+        db_context = GetAccountDetails(token['userId'])
+    elif intent == "Transactions":
         # db_context = GetTransactionHistory(token['localId'])
         pass
-    elif intent == "Interest_rates":
+    elif intent == "savings_rates":
         # db_context = GetInterestRates()
         pass
-    elif intent == "Loan_rates":
+    elif intent == "fixed_rates":
+        # db_context = GetInterestRates()
+        pass
+    elif intent == "loan_rates":
         # db_context = GetLoanRates()
         pass
-    elif intent == "Exchange_rates":
+    elif intent == "exchange_rates":
         # db_context = GetExchangeRates()
         pass
     elif intent == 'vectorDb':
         vec_db_context = search_similarity(user_msg)
 
     past_msgs = dict()
-
+    
     llm_response = generate_llama2_response(user_msg, past_msgs=past_msgs, context=vec_db_context, db_ans=db_context)
+
+    return llm_response
